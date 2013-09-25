ba1e9b71ef452a63da4d80e7ee7ef837225c00ed
Sajith Silva <sajith.silva@pagero.com>
2013-09-25 06:15:49 +0530

    added missing classes

---
+++ FacebookFriends/src/com/example/facebookfriends/model/Friend.java
@@ -0,0 +1,15 @@
+package com.example.facebookfriends.model;
+
+
+public class Friend {
+    private String name;
+
+    public Friend(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
---
+++ FacebookFriends/src/com/example/facebookfriends/model/FriendGroup.java
@@ -0,0 +1,14 @@
+package com.example.facebookfriends.model;
+
+
+public class FriendGroup {
+    private String name;
+
+    public FriendGroup(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
